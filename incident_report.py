8166351b491cbe6eebae7f32848c364c434ea337
sydelise <sydelise2029@gmail.com>
2026-09-18 21:12:39 -0500

    add incident response details

--- incident_report.py
+++ incident_report.py
@@ -9,12 +9,17 @@ time = input("Enter a time: ")
 device = input("Enter a device (include a or an): ")
 location = input("Enter a location (include the or a): ")
 suspicious_item = input("Enter a suspicious object (include a or an): ")
+severity = input("Enter a dramatic severity level: ")
+recommended_action = input("Enter a recommended action: ")
+
+
 
 incident_summaries = [
     f"At {time}, {device} in {location} attempted to communicate with {suspicious_item}.",
     f"At {time}, staff in {location} discovered {suspicious_item} controlling {device}.",
     f"Security cameras in {location} recorded {device} following {suspicious_item} at {time}.",
     f"A report from {location} claims that {suspicious_item} tried to log in to {device} at {time}.",
+   
 ]
 
 summary = random.choice(incident_summaries)
@@ -22,3 +27,5 @@ summary = random.choice(incident_summaries)
 print()
 print("INCIDENT SUMMARY")
 print(summary)
+print(f"Severity: {severity}")
+print(f"Recommended Action: {recommended_action}")
